a5555e54af5382eade3b2ac73ef07adec8ebd0aa
Dengjianping <djptux@gmail.com>
2016-10-15 23:35:26 +0800

    updated readme and implemented addition

--- Addition/Addition/main.cu
+++ Addition/Addition/main.cu
@@ -2,80 +2,77 @@
 #include "cuda_runtime.h"
 #include "device_launch_parameters.h"
 
-#include <thrust/host_vector.h>
-#include <thrust/device_vector.h>
-
 #include <iostream>
-#include <cmath>
-#include <vector>
-#include <ctime>
+#include <chrono>
 #include <stdio.h>
 
 using namespace std;
 
-const int N = 2;
+const int N = 10000;
 
-__global__ void addition(int **c, int **a, int **b, const int N)
+__global__ void addition(int *c, int *a, int *b, const int N)
 {
-	int i = blockDim.y*blockIdx.y + threadIdx.y;
-	int j = blockDim.x*blockIdx.x + threadIdx.x;
-	//printf("index: %d, %d\n", i, j);
-	printf("index: %d, %d\n", a[i][j], b[i][j]);
-	c[i][j] = a[i][j] + b[i][j];
-    if (i < N && j < N)
+    int i = blockDim.x*blockIdx.x + threadIdx.x;
+    if (i < N )
     {
-		printf("index: %d, %d\n", a[i][j], b[i][j]);
-        c[i][j] = a[i][j] + b[i][j];
+        c[i] = a[i] + b[i];
     }
-}
-
-__device__ int printIdx()
-{
-	return 10;
 }
 
 int generateRandomNum(bool bigger)
 {
     if (!bigger)
     {
-        return rand() % 10;     
+        return rand() % 10; // range from 0 to 10
     }
     else 
     {
-        return rand() % 100;
+        return rand() % 100; // range from 0 to 10
     }
 }
 
-void initVec(int a[N][N], const int N, bool bigger=false)
+void initVec(int a[N], const int N, bool bigger=false)
 {
+    cout << "input array: ";
     for (int i = 0; i < N; i++)
     {
-		for (size_t j = 0; j < N; j++)
-		{
-			a[i][j] = generateRandomNum(bigger);
-			cout << a[i][j] << endl;
-		}
+        a[i] = generateRandomNum(bigger);
+        //cout << a[i] << ", ";
+    }
+    cout << endl << "------------" << endl;
+}
+
+void showArray(int *a, const int N)
+{
+    cout << "output array: ";
+    for (size_t i = 0; i < N; i++)
+    {
+        cout << a[i] << ", ";
+    }
+    cout << endl << "------------" << endl;
+}
+
+void arrayAdditionOnCPU(int *c, int *a, int *b, const int N)
+{
+    for (size_t i = 0; i < N; i++)
+    {
+        c[i] = a[i] + b[i];
     }
 }
 
 int main()
 {
-	
-	// host data
-	int a[N][N];
+    
+    // host data
+    int a[N], b[N], c[N], d[N];
     initVec(a, N);
-
-	cout << "--------------" << endl;
     
-    int b[N][N];
     initVec(b, N, true);
-    
-    int c[N][N];
-    
-	int size = N * N * sizeof(int);
+    int size = N * sizeof(int);
     
     // device data
     int *d_a, *d_b, *d_c;
+    // allocate space for devece data
     cudaMalloc((void **)&d_a, size);
     cudaMalloc((void **)&d_b, size);
     cudaMalloc((void **)&d_c, size);
@@ -85,21 +82,43 @@ int main()
     cudaMemcpy(d_b, b, size, cudaMemcpyHostToDevice);
     
     // define threads and blocks
-	dim3 threadPerBlock(1, 1); // just 1*1 thread in a block
-	dim3 blockSize(2, 2); // 2*2 blocks
+    dim3 threadPerBlock(2); // just 1 thread in a block
+    dim3 blockSize(N / threadPerBlock.x); // 2 blocks
 
-    addition <<< blockSize, threadPerBlock >>> (&d_c, &d_a, &d_b, N);
+    cudaEvent_t start, stop;
+    float deviceTimeCost;
+
+    cudaEventCreate(&start);
+    cudaEventCreate(&stop);
+
+    cudaEventRecord(start);
+    addition<<<blockSize, threadPerBlock>>>(d_c, d_a, d_b, N);
+    cudaEventRecord(stop);
+
+    cudaEventSynchronize(stop);
+    
+    cudaEventElapsedTime(&deviceTimeCost, start, stop);
+    //destroy all event
+    cudaEventDestroy(start);
+    cudaEventDestroy(stop);
 
     cudaMemcpy(c, d_c, size, cudaMemcpyDeviceToHost);
-    // device memory free
+    // free device memory
     cudaFree(d_a);
     cudaFree(d_b);
     cudaFree(d_c);
 
-	cudaDeviceSynchronize();
-	system("pause");
+    cout << "Time Consumed on GPU: " << deviceTimeCost << endl;
 
-    cout << c[1][1] << endl;
+    auto hostStart = chrono::steady_clock::now();
+    arrayAdditionOnCPU(d, a, b, N);
+    auto hostEnd = chrono::steady_clock::now();
+    float hostTimeCost = chrono::duration_cast<chrono::duration<float> >(hostEnd - hostStart).count();
+    cout << "Time Consumed on Host: " << hostTimeCost << endl;
+
+    cout << "who is faster: " << deviceTimeCost / hostTimeCost << endl;
+
+    //showArray(c, N);
     system("pause");
 
     return 0;
